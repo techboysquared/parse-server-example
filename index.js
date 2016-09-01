2530ef45fb0d3a13b48b35b767e0c2aa338cdafd
techboysquared <cody95@vt.edu>
2016-08-31 21:32:39 -0700

    Added quick fix

--- index.js
+++ index.js
@@ -27,6 +27,19 @@ var api = new ParseServer({
 
 var app = express();
 
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'accept, content-type, x-parse-application-id, x-parse-rest-api-key, x-parse-session-token');
+     // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+});
+
 // Serve static assets from the /public folder
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
